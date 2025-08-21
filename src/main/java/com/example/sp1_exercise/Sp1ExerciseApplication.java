565bfe290dd2ddadf8f4837b909360aee21d565a
Janne <bhi256@myy.haaga-helia.fi>
2025-08-21 20:33:27 +0300

    new project

---
+++ src/main/java/com/example/sp1_exercise/Sp1ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sp1_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp1ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp1ExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sp1_exercise/Sp1ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sp1_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp1ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
